fa73e27f2919475762081b4e0953c73445465696
Daniel VanRyn <daniel.vanryn@mymail.champlain.edu>
2022-02-01 18:34:17 -0500

    Initial commit

---
+++ ProjTest/Source/ProjTest/ProjTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJTEST_API AProjTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
